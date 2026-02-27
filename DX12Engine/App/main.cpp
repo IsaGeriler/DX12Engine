bccc55822bfa5350aa3c0d62a0438f7d476f112a
Isa Berk Geriler <gerilerisaberk@gmail.com>
2026-02-27 14:34:45 +0000

    Red screen drawn on the window (GPU)

--- DX12Engine/App/main.cpp
+++ DX12Engine/App/main.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 
 #include "../Platform/Window.h"
+#include "../Graphics/DX12Core.h"
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ PSTR lpCmdLine, _In_ int nCmdShow) {
 	AllocConsole();
@@ -10,10 +11,17 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 	Window window;
 	window.initialize("DX12Engine", 1024, 1024);
 
+	DX12Core core;
+	core.initialize(window.hwnd, 1024, 1024);
+
 	while (true) {
+		core.beginFrame();
 		window.processMessages();
+		if (window.keys[VK_ESCAPE]) break;
+		core.finishFrame();
 	}
 
+	core.flushGraphicsQueue();
 	FreeConsole();
 	return 0;
 }
